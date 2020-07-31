dcca4480834005e318dbae2221aac20182b44603
Veer Gadodia <veergadodia24@gmail.com>
2020-07-31 19:45:19 -0400

    initial commit web app

---
+++ app.py
@@ -0,0 +1,37 @@
+"""Web app."""
+import flask
+from flask import Flask, render_template, request, redirect, url_for
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+@app.route('/index.html')
+def index() -> str:
+    """Base page."""
+    return flask.render_template("index.html")
+
+@app.route('/plots.html')
+def plots():
+    return render_template('plots.html')
+
+@app.route('/binary.html')
+def binary():
+    return render_template('binary.html')
+
+@app.route('/heatmaps.html')
+def heatmaps():
+    return render_template('heatmaps.html')
+
+@app.route('/predicts.html')
+def predicts():
+    return render_template('predicts.html')
+
+@app.route('/predicts.html', methods=["GET", "POST"])
+def get_predicts():
+    firstname = request.form["firstname"]
+    print(firstname) 
+    return render_template('predicts.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
